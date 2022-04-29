e43486dacb0af7c22bc2505d802b7b31b1806b52
blessedjasonmwanza <mwanzabj@gmail.com>
2022-04-29 21:41:27 +0200

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'Posts', type: :request do
       get '/users/1/posts'
       expect(response.body).to include('Posts by user Page')
     end
-    it "shows post by id" do
+    it 'shows post by id' do
       get '/users/1/posts/1'
       expect(response).to have_http_status(200)
       expect(response.body).to include('Show post by id page')
