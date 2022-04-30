b52b70594e3a2ef2fdafea28776e491d87849b0f
blessedjasonmwanza <mwanzabj@gmail.com>
2022-04-30 13:15:02 +0200

    Use reference column type for foreign key references

--- db/migrate/20220430104019_create_comments.rb
+++ db/migrate/20220430104019_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :author_id, null: false, index: true, foreign_key: { to_table: :users }
-      t.integer :post_id, null: false, index: true, foreign_key: { to_table: :posts }
+      t.references :author_id, null: false, index: true, foreign_key: { to_table: :users }
+      t.references :post_id, null: false, index: true, foreign_key: { to_table: :posts }
       t.text :text
 
       t.timestamps
--- db/migrate/20220430104229_create_likes.rb
+++ db/migrate/20220430104229_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :author_id, null: false, index: true, foreign_key: { to_table: :users }
-      t.integer :post_id, null: false, index: true, foreign_key: { to_table: :posts }
+      t.references :author_id, null: false, index: true, foreign_key: { to_table: :users }
+      t.references :post_id, null: false, index: true, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
